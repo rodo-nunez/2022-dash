be22d6c68dcee145164f18f5e9885864532ed9a6
Rodo-ASUS <rununez@entel.cl>
2024-02-06 13:33:14 -0300

    part 1 - step 1

--- part1/main.py
+++ part1/main.py
@@ -1,14 +1,5 @@
-from dash import Dash
-from dash_bootstrap_components.themes import BOOTSTRAP
-
-from src.components.layout import create_layout
-
-
 def main() -> None:
-    app = Dash(external_stylesheets=[BOOTSTRAP])
-    app.title = "Medal dashboard"
-    app.layout = create_layout(app)
-    app.run()
+    pass
 
 
 if __name__ == "__main__":
