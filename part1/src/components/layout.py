5942979cfbb2566fee8672120ca8c070d604b3ec
Rodo-ASUS <rununez@entel.cl>
2024-02-06 14:06:16 -0300

    parte 1 - step 5 - dropdown nations

--- part1/src/components/layout.py
+++ part1/src/components/layout.py
@@ -1,10 +1,18 @@
 from dash import Dash, html
 
+from . import bar_chart, nation_dropdown
+
 def create_layout(app: Dash) -> html.Div:
     return html.Div(
         className="app-div",
         children=[
             html.H1(app.title),
-            html.Hr()
+            html.Hr(),
+            html.Div(
+                className="dropdown-container",
+                children=[
+                    nation_dropdown.render(app),
+                ],
+            )
         ]
     )
