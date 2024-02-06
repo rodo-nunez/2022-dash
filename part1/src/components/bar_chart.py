ca7a8faf4eccb1aae623e6787791cad0f44d33c8
Rodo-ASUS <rununez@entel.cl>
2024-02-06 14:53:55 -0300

    part 1 - step 9 - callback for bar chart

--- part1/src/components/bar_chart.py
+++ part1/src/components/bar_chart.py
@@ -7,6 +7,18 @@ from src.components import ids
 MEDAL_DATA = px.data.medals_long()
 
 def render(app: Dash) -> html.Div:
-    
-    fig = px.bar(MEDAL_DATA, x="medal", y="count", color="nation", text="nation")
-    return html.Div(dcc.Graph(figure=fig), id=ids.BAR_CHART)
+
+    @app.callback(
+        Output(ids.BAR_CHART, "children"),
+        [
+            Input(ids.NATION_DROPDOWN, "value"),
+        ],
+    )
+    def update_bar_chart(nations: list[str]) -> html.Div:
+
+        fig = px.bar(MEDAL_DATA, x="medal", y="count",
+                     color="nation", text="nation")
+
+        return html.Div(dcc.Graph(figure=fig), id=ids.BAR_CHART)
+
+    return html.Div(id=ids.BAR_CHART)
