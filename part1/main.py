d69bcac8e0cb1e3086155446fb81f25fd17d6852
Rodo-ASUS <rununez@entel.cl>
2024-02-06 13:54:55 -0300

    part 1 - step 3

--- part1/main.py
+++ part1/main.py
@@ -1,9 +1,10 @@
 from dash import Dash, html
+from src.components.layout import create_layout
 
 def main() -> None:
     app = Dash()
     app.title = "Medal dashboard"
-    app.layout = html.Div()
+    app.layout = create_layout(app)
     app.run()
 
 
