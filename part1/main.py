5cc001b09b7873627e7abc22d89e8a31593847ca
Rodo-ASUS <rununez@entel.cl>
2024-02-06 13:56:21 -0300

    part 1 - step 4 - bootstrap

--- part1/main.py
+++ part1/main.py
@@ -1,8 +1,10 @@
 from dash import Dash, html
+from dash_bootstrap_components.themes import BOOTSTRAP
+
 from src.components.layout import create_layout
 
 def main() -> None:
-    app = Dash()
+    app = Dash(external_stylesheets=[BOOTSTRAP])
     app.title = "Medal dashboard"
     app.layout = create_layout(app)
     app.run()
